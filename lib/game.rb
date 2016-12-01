029d88e5ed9753801ae631e301075cbcbb2ece9d
glynester <glynester@hotmail.com>
2016-12-01 13:00:50 +0000

    Player can make a move. Move is recorded in log. Player may not go twice in a row.

--- lib/game.rb
+++ lib/game.rb
@@ -2,14 +2,24 @@ require_relative '../lib/player'
 
 class Game
 
-  attr_reader :player1, :player2
+  attr_reader :player1, :player2, :moves, :log
 
   def initialize(player1 = "Player1", player2 = "Player2")
    player1.is_a?(Player) ? @player1 = player1 : @player1 = Player.new(player1)
    player2.is_a?(Player) ? @player2 = player2 : @player2 = Player.new(player2)
-   row3_col1=""; row3_col2=""; row3_col3="";
-   row2_col1=""; row2_col2=""; row2_col3="";
-   row1_col1=""; row1_col2=""; row1_col3="";
+   :r3c1; :r3c2; :r3c3;
+   :r2c1; :r2c2; :r2c3;
+   :r1c1; :r1c2; :r1c3;
+   @moves = []
+   @log = ""
+  end
+
+  def move(player, position)
+    if moves.count > 0
+      raise "Not your go" if (@moves.last[0].name == player.name)   #@moves.last[].name
+    end
+    @moves << [player, position]
+    @log << "#{player.name} just moved to #{position}\n"
   end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,6 +3,11 @@ require_relative '../lib/game'
 
 describe Game do
 
+  let(:p1) {double(:player1, name: "Player1")}
+  let(:p2) {double(:player2, name: "Player2")}
+
+
+
   context "start a new game" do
     it 'has 2 players initialized by default as player1 and player2' do
       g1 = Game.new
@@ -23,4 +28,23 @@ describe Game do
     end
 
   end
+
+  context "make a move" do
+
+    it 'shows who has made the move and what the move is' do
+      g1 = Game.new("Fred", "George")
+      g1.move(g1.player1,"r1c1")
+      expect(g1.moves[0]).to eq [g1.player1, "r1c1"]
+    end
+
+    it 'raises an error if someone moves twice' do
+      g1 = Game.new("Fred", "George")
+      g1.move(g1.player1,"r1c1")
+      expect{g1.move(g1.player1,"r1c2")}.to raise_error (RuntimeError)
+
+    end
+
+
+  end
+
 end
