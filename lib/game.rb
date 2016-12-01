69635d0e53494bfd5a010545a4e8d18849569e4f
glynester <glynester@hotmail.com>
2016-12-01 14:59:58 +0000

    Added winning positions array

--- lib/game.rb
+++ lib/game.rb
@@ -51,6 +51,10 @@ class Game
   end
 
   def check_for_winner(player)
+    winning_position = [[:r1c1, :r1c2, :r1c3],[:r2c1, :r2c2, :r2c3],[:r3c1, :r3c2, :r3c3,],
+    [:r1c1,:r2c1,:r3c1],[:r1c2,:r2c2,:r3c2],[:r1c3,:r2c3,:r3c3],
+    [:r1c1,:r2c2,:r3c3],[:r3c1,:r2c2,:r1c3]]
+
 
   end
 
